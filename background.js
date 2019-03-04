68316f29ce4f82784a78f08b4f48c5de8222e192
Rudy <jiram22@comcast.net>
2019-03-04 05:35:34 -0800

    inital commit, basic posts scrape

--- background.js
+++ background.js
@@ -10,5 +10,46 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is green.");
   });
 
+  //get nbaredditstreams json
+  let json = httpGet("https://www.reddit.com/r/nbastreams/hot.json")
+
+  //parse json
+  let data = JSON.parse(json).data
+
+  //get posts
+  let posts = data.children
+
+
+  //get title and url for each post
+  posts.forEach((post)=>{
+  	console.log(post.data.title)
+  	console.log(post.data.url)
+
+  	/*
+  	scrap title for nba game
+		if nba game post
+			get post url
+			scrape post for stream links
+			store nba game match up and stream links
+			
+		*/
+  })
+
   
 });
+
+function httpGet(theUrl){
+  var xmlHttp = null;
+
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl,false)
+  xmlHttp.onreadystatechange=function()
+  {
+      if (xmlHttp.readyState==4 && xmlHttp.status==200)
+      {
+          return xmlHttp.responseText;
+      }
+  }
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
